40d84eb906425410eef05a05af3bb7924da6e2c1
Sergey Matyushenok <matyushenoksergei@yandex.by>
2024-02-18 17:18:02 +0100

    add auth

--- app/core/config.py
+++ app/core/config.py
@@ -1,2 +1,6 @@
 BOT_TOKEN = ""
 BOT_MINI_APP_URL = "https://zum-utah-load-bibliography.trycloudflare.com/"
+
+# Replace YOUR_API_KEY and YOUR_API_SECRET with your actual credentials
+BINANCE_API_KEY = ""
+BINANCE_API_SECRET = ""
--- app/core/modules/binance/client.py
+++ app/core/modules/binance/client.py
@@ -1,21 +1,43 @@
-from typing import List, Dict
+import hashlib
+import hmac
+import time
+from typing import Dict, List
 
 import requests
 
+from app.core.config import BINANCE_API_KEY, BINANCE_API_SECRET
+
 
 class BinanceClient:
-    def __init__(self, api_url: str):
+    def __init__(self, api_url: str, api_key: str, api_secret: str):
         self.api_url = api_url
+        self.api_key = api_key
+        self.api_secret = api_secret
+
+    def get_auth_headers(self):
+        timestamp = int(round(time.time() * 1000))
+        query_string = f"timestamp={timestamp}"
+        signature = hmac.new(
+            self.api_secret.encode("utf-8"), query_string.encode("utf-8"), hashlib.sha256
+        ).hexdigest()
+        headers = {
+            "X-MBX-APIKEY": self.api_key,
+            "X-MBX-TIMESTAMP": str(timestamp),
+            "X-MBX-SIGNATURE": signature,
+        }
+        return headers
 
     def get_available_tickers(self):
+        headers = self.get_auth_headers()
         url = self.api_url + "/api/v3/exchangeInfo"
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()  # Raise an exception for error status codes
         data = response.json()
         usdt_symbols = [symbol['symbol'] for symbol in data['symbols'] if symbol['symbol'].endswith("USDT")]
         return usdt_symbols
 
     def get_daily_pnl(self, assets_names: List[str]):
+        headers = self.get_auth_headers()
         usdt_symbols = self.get_available_tickers()
         filtered_assets_names = [symbol for symbol in assets_names if f"{symbol}USDT" in usdt_symbols]
         if not filtered_assets_names:
@@ -24,7 +46,7 @@ class BinanceClient:
         for symbol in filtered_assets_names:
             url += f"\"{symbol}USDT\","
         url = url[:-1] + "]"
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()  # Raise an exception for error status codes
         data = response.json()
 
@@ -37,6 +59,7 @@ class BinanceClient:
         return usdt_symbols
 
     def get_assets_prices(self, assets_names: List[str]) -> Dict[str, int]:
+        headers = self.get_auth_headers()
         usdt_symbols = self.get_available_tickers()
         filtered_assets_names = [symbol for symbol in assets_names if f"{symbol}USDT" in usdt_symbols]
         if not filtered_assets_names:
@@ -45,7 +68,7 @@ class BinanceClient:
         for symbol in filtered_assets_names:
             url += f"\"{symbol}USDT\","
         url = url[:-1] + "]"
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()  # Raise an exception for error status codes
 
         data = response.json()
@@ -56,6 +79,10 @@ class BinanceClient:
 
 
 binance_client = BinanceClient(
-    api_url='https://api.binance.com'
+    api_url='https://api.binance.com',
+    api_key=BINANCE_API_KEY,
+    api_secret=BINANCE_API_SECRET,
+
 )
-# binance_client.get_daily_pnl(["BTC", 'LTC'])
+# a = binance_client.get_daily_pnl(["BTC", 'LTC'])
+# print(a)
